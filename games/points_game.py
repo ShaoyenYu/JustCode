566163b7411a279f3becf29da1b014a88aa1a118
Yu <shaoyenyu@gmail.com>
2017-12-19 13:27:53 +0800

    # 24点解法更新;

--- games/points_game.py
+++ games/points_game.py
@@ -1,35 +1,22 @@
-class PointsGame:
+
+class Solution:
     @classmethod
-    def setup(cls):
+    def _setup(cls):
         pass
 
     @classmethod
-    def points_gmae(cls):
-        pass
-
-    @classmethod
-    def run(cls):
+    def _run(cls):
         cls.points_game()
 
     @classmethod
     def main(cls):
-        cls.setup()
-        cls.run()
+        cls._setup()
+        cls._run()
 
 
-class Solution1(PointsGame):
+class PointsGame1(Solution):
     @classmethod
-    def setup(cls):
-        import numpy as np
-        from array import array
-        cls.CARD_NUMBER = 4
-        cls.RESULT_VALUE = 24
-        cls.case = np.random.randint(1, 14, size=4)
-        cls.nums = array("d", cls.case)
-        cls.ress = list(cls.case)
-
-    @classmethod
-    def points_game(cls, n):
+    def points_game(cls, n: int):
         """
         Beauty of Programming 24点游戏(***, P103) Solution.1
         """
@@ -49,6 +36,11 @@ class Solution1(PointsGame):
 
                 cls.ress[i] = f"({expa}+{expb})"
                 cls.nums[i] = a + b
+                if cls.points_game(n - 1):
+                    return True
+
+                cls.ress[i] = f"({expa}-{expb})"
+                cls.nums[i] = a - b
                 if cls.points_game(n - 1):
                     return True
 
@@ -80,24 +72,68 @@ class Solution1(PointsGame):
         return False
 
     @classmethod
-    def run(cls):
+    def _setup(cls):
+        import numpy as np
+        from array import array
+        cls.CARD_NUMBER = 4
+        cls.RESULT_VALUE = 24
+        cls.case = np.random.randint(1, 14, size=4)
+        cls.case = [1, 13, 8, 3]
+        cls.nums = array("d", cls.case)
+        cls.ress = list(cls.case)
+
+    @classmethod
+    def _run(cls):
         print(f"CASE:{cls.case}")
         print(f"RESULT: {cls.points_game(cls.CARD_NUMBER), cls.ress[0]}")
 
 
-class Solution2(PointsGame):
+class PointsGame2(Solution):
+
     @classmethod
-    def points_game(cls, numbers, value):
+    def _setup(cls):
+        import numpy as np
+        cls.RESULTS = []
+        cls.case = np.random.randint(1, 14, size=4)
+        cls.case = [1, 13, 8, 3]
+
+    @classmethod
+    def _run(cls):
+        print(cls.case)
+        cls.points_game(cls.case)
+        print(cls.RESULTS)
+
+    @classmethod
+    def points_game(cls, numbers, exprs=""):
+        """所有解"""
         if len(numbers) == 1:
             if numbers[0] == 24:
-                return True
+                cls.RESULTS.append(exprs)
             else:
-                return False
-        for i in range(len(numbers)):
-            for j in range(i + 1, len(numbers)):
+                pass
+
+        length = len(numbers)
+        for i in range(length):
+            if length == 3:
+                exprs = numbers[i]
+
+            for j in range(i + 1, length):
                 a, b = numbers[i], numbers[j]
+                remain_list = [numbers[x] for x in range(length) if (x != i and x != j)]
+
+                cls.points_game([a + b, *remain_list], f"({exprs}+{int(b)})")
+                cls.points_game([a - b, *remain_list], f"({exprs}-{int(b)})")
+                cls.points_game([a * b, *remain_list], f"({exprs}*{(b)})")
+                if b != 0:
+                    cls.points_game([a / b, *remain_list], f"({exprs}/{int(b)})")
+                if a != 0:
+                    cls.points_game([b / a, *remain_list], f"({int(b)}/{exprs})")
 
 
 def main():
-    Solution1.main()
-    # Solution2.main()
+    PointsGame1.main()
+    PointsGame2.main()
+
+
+if __name__ == "__main__":
+    main()
