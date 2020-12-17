6c8cce02df42283dd6900b5135adbd35bc97d626
ShaoyenYu <shaoyenyu@gmail.com>
2020-12-17 23:56:15 +0800

    [BLD] add another solution for Q169

--- alogs/leetcode/solutions/Q0169_Majority_Element/solution.py
+++ alogs/leetcode/solutions/Q0169_Majority_Element/solution.py
@@ -8,14 +8,25 @@ class Solution:
 
 class Solution01(Solution):
     def majorityElement(self, nums: List[int]) -> int:
+        # using Boyer-Moore Voting Algorithm
         i, cur = 0, nums[0]
         for num in nums:
-            if cur == num:
+            if i == 0:
+                i, cur = 1, num
+            elif cur == num:
                 i += 1
             else:
                 i -= 1
+        return num
 
-            if i == 0:
-                cur = num
-                i += 1
-        return cur
+
+class Solution02(Solution):
+    def majorityElement(self, nums: List[int]) -> int:
+        # using hash map to count
+        count = {}
+        threshold = len(nums) // 2 + 1
+        for num in nums:
+            val = count.get(num, 0) + 1
+            if val == threshold:
+                return num
+            count[num] = val
