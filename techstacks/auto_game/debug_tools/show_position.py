2d9614400485218008b4674e1100aef7e3e02976
ShaoyenYu <shaoyenyu@gmail.com>
2021-12-20 16:19:06 +0800

    [ENH] refactor `techstacks.auto_game.debug_tools.show_position` script --- + implement `PixelDetector` class, to manage pixel and monitor related methods and property; + implement `PausableThread` class, using `Thread.Event` to implement pause and resume; + use a main thread to handle user input, can control the job;

--- techstacks/auto_game/debug_tools/show_position.py
+++ techstacks/auto_game/debug_tools/show_position.py
@@ -1,100 +1,153 @@
 import time
-from threading import Thread
+from queue import Queue
+from threading import Thread, Event
 
 import pyautogui as pag
-import win32gui
 from pynput import keyboard
 
-from util.win32 import monitor, common
+from util.win32 import common, monitor, win32gui
+from util.win32.window import parse_rbg_int2tuple
 
-monitor.set_process_dpi_awareness(1, silent=True)
-
-
-class Pixel:
-    pos_client = None
-    rgb_tuple = None
-    rgb_int = None
+monitor.set_process_dpi_awareness(2, silent=True)
 
 
-class Config:
-    sleep_time = 1
-    paused = False
+class PixelDetector:
+    monitors = monitor.get_monitors()
+    config = {"adjust_coordinate": True}
+    cur_pixel_info = dict()
+
+    @classmethod
+    def adjust_coordinate(cls, x, y):
+        for mon in cls.monitors:
+            if mon.rect.left <= x < mon.rect.right and mon.rect.top <= y <= mon.rect.bottom:
+                cls.cur_pixel_info["monitor"] = mon
+                return x - mon.rect.left, y - mon.rect.top
+
+    @classmethod
+    def get_pixel_rgb(cls, coordinate, hwnd):
+        hdc = win32gui.GetWindowDC(hwnd)
+        try:
+            rgb_int = win32gui.GetPixel(hdc, *coordinate)
+        except:
+            rgb_int = None
+        finally:
+            win32gui.ReleaseDC(hwnd, hdc)
+
+        return rgb_int
+
+    @classmethod
+    def update_pixel_info(cls):
+        p = pag.position()
+        coordinate_global = p.x, p.y
+
+        hwnd_client = win32gui.WindowFromPoint(coordinate_global)
+        coordinate_client = win32gui.ScreenToClient(hwnd_client, coordinate_global)
+
+        rgb_int = cls.get_pixel_rgb(coordinate_client, hwnd_client)
+        rgb_tuple = parse_rbg_int2tuple(rgb_int) if rgb_int is not None else None
+
+        cls.cur_pixel_info.update(
+            hwnd_client=hwnd_client,
+            rgb_int=rgb_int, rgb_tuple=rgb_tuple,
+            coordinate_global=coordinate_global, coordinate_client=coordinate_client,
+            coordinate_global_mod=cls.adjust_coordinate(p.x, p.y),
+        )
+
+    @classmethod
+    def tell_pixel_info(cls):
+        def pretty_format(it, space):
+            return ", ".join((f"{s:>{space}}" for s in it))
+
+        pixel_info = cls.cur_pixel_info.copy()
+        hwnd_client = pixel_info['hwnd_client']
+        coordinate_global_to_use = "coordinate_global_mod" if cls.config["adjust_coordinate"] else "coordinate_global"
+        text_coordinate_global = pretty_format(pixel_info[coordinate_global_to_use], 4)
+        text_coordinate_client = pretty_format(pixel_info["coordinate_client"], 4)
+        text_rgb_tuple = pretty_format(pixel_info["rgb_tuple"] or ('', '', ''), 3)
+
+        print("=" * 128)
+        print(f"【{win32gui.GetWindowText(hwnd_client)}】@{hwnd_client} @{pixel_info['monitor']}")
+        print(f"Global: ({text_coordinate_global})  "
+              f"Client: ({text_coordinate_client})  "
+              f"RGB: ({text_rgb_tuple}) {rgb_int if (rgb_int := pixel_info['rgb_int']) is not None else ''}")
+
+    @classmethod
+    def copy_pixel_info(cls):
+        pixel_info = cls.cur_pixel_info.copy()
+        common.set_text(f"{[*pixel_info['coordinate_client'], pixel_info['rgb_int']]},")
 
 
-def transform_rbg_int2tuple(rgb_int: int):
-    return rgb_int & 0xff, (rgb_int >> 8) & 0xff, (rgb_int >> 16) & 0xff
+class PausableThread(Thread):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.status_can_run = Event()
+        self.interval = 1
 
+    def start(self) -> None:
+        super().start()
+        self.resume()
 
-def transform_rbg_tuple2int(rgb_tuple: tuple):
-    return rgb_tuple[0] << 16 | rgb_tuple[1] << 8 | rgb_tuple[2]
-
-
-def show_position_and_client():
-    while True:
-        if not Config.paused:
-            p = pag.position()
-            pos_global = (p.x, p.y)
-            hwnd = win32gui.WindowFromPoint(pos_global)
-            Pixel.pos_client = win32gui.ScreenToClient(hwnd, pos_global)
-            hdc = win32gui.GetWindowDC(hwnd)
+    def run(self) -> None:
+        while True:
+            self.status_can_run.wait()
             try:
-                Pixel.rgb_int = win32gui.GetPixel(hdc, *Pixel.pos_client)
-                Pixel.rgb_tuple = transform_rbg_int2tuple(Pixel.rgb_int)
-            except:
-                Pixel.rgb_int = ""
-                Pixel.rgb_tuple = ("", "", "")
+                PixelDetector.update_pixel_info()
+                PixelDetector.tell_pixel_info()
+                time.sleep(self.interval)
             finally:
-                win32gui.ReleaseDC(hwnd, hdc)
+                pass
 
-            pos_global_str = f", ".join((f"{s:>4}" for s in pos_global))
-            pos_client_str = f", ".join((f"{s:>4}" for s in Pixel.pos_client))
-            rgb_client_str = f", ".join((f"{s:>3}" for s in Pixel.rgb_tuple))
+    def pause(self):
+        self.status_can_run.clear()
+        print("paused")
 
-            print("=" * 128)
-            print(f"【{win32gui.GetWindowText(hwnd)}】@{hwnd}")
-            print(f"Global: ({pos_global_str})  Client: ({pos_client_str})  RGB: ({rgb_client_str}) {Pixel.rgb_int}")
+    def resume(self):
+        self.status_can_run.set()
+        print("resumed")
 
-            time.sleep(Config.sleep_time)
-        else:
-            time.sleep(1)
+    def is_paused(self):
+        return not self.status_can_run.is_set()
+
+    def set_interval(self):
+        self.status_can_run.clear()
+        self.interval = float(input("set a new interval: "))
+        self.status_can_run.set()
 
 
-class EventHandler:
+class Manager(Thread):
     def __init__(self):
-        self._listener_keyboard = None
+        super().__init__()
+        self.input_keys = Queue()
+        self._job_thread = PausableThread()
+        self._listener_keyboard = keyboard.Listener(on_release=self.on_keyboard_release)
 
     def start(self):
-        self._listener_keyboard = keyboard.Listener(on_release=self.on_keyboard_release)
+        super().start()
+        self._job_thread.start()
         self._listener_keyboard.start()
 
+    def run(self) -> None:
+        binding_ops = {
+            "f4": PixelDetector.copy_pixel_info,
+            "f8": self._job_thread.set_interval,
+            "f12": lambda: self._job_thread.resume() if self._job_thread.is_paused() else self._job_thread.pause(),
+        }
+
+        while True:
+            key = self.input_keys.get(block=True)
+            key_name = self.get_key_name(key)
+            if (op := binding_ops.get(key_name)) is not None:
+                print(f"{key_name} pressed")
+                op()
+
+    @staticmethod
+    def get_key_name(key):
+        return key.name if hasattr(key, 'name') else key
+
     def on_keyboard_release(self, key):
-        if key is keyboard.Key.f4:
-            print("f4 is pressed, copy...")
-            common.set_text(f"{[*Pixel.pos_client, Pixel.rgb_int]},")
-
-        elif key is keyboard.Key.f8:
-            Config.paused = True
-            Config.sleep_time = float(input("Input a new sleep time:\n"))
-            Config.paused = False
-
-        elif key is keyboard.Key.f10:
-            Config.paused = not Config.paused
-            if Config.paused:
-                print("Paused")
-            else:
-                print("Continued")
-
-        elif key is keyboard.Key.f12:
-            return False
-
-
-def main():
-    eh = EventHandler()
-    eh.start()
-
-    w1 = Thread(target=show_position_and_client())
-    w1.start()
+        self.input_keys.put(key, block=False)
 
 
 if __name__ == '__main__':
-    main()
+    job = Manager()
+    job.start()
