548c7a12aef7d2921f88672bb85c9ea4499cd08b
ShaoyenYu <shaoyenyu@gmail.com>
2022-01-05 20:25:52 +0800

    [ENH] add another implemetation to `PixelDetector.get_pixel_rgb`, to bypass the problem that pixel of some window cannot be directly accessed by win32api;

--- techstacks/auto_game/debug_tools/show_position.py
+++ techstacks/auto_game/debug_tools/show_position.py
@@ -6,14 +6,14 @@ import pyautogui as pag
 from pynput import keyboard
 
 from util.win32 import common, monitor, win32gui
-from util.win32.window import parse_rbg_int2tuple
+from util.win32.window import parse_rbg_int2tuple, parse_rbg_tuple2int
 
 monitor.set_process_dpi_awareness(2, silent=True)
 
 
 class PixelDetector:
     monitors = monitor.get_monitors()
-    config = {"adjust_coordinate": True}
+    config = {"adjust_coordinate": True, "method": 1}
     cur_pixel_info = dict()
 
     @classmethod
@@ -26,12 +26,20 @@ class PixelDetector:
     @classmethod
     def get_pixel_rgb(cls, coordinate, hwnd):
         hdc = win32gui.GetWindowDC(hwnd)
-        try:
-            rgb_int = win32gui.GetPixel(hdc, *coordinate)
-        except:
-            rgb_int = None
-        finally:
-            win32gui.ReleaseDC(hwnd, hdc)
+        if cls.config["method"] == 1:
+            try:
+                rgb_int = win32gui.GetPixel(hdc, *coordinate)
+                win32gui.ReleaseDC(hwnd, hdc)
+            except:
+                win32gui.ReleaseDC(hwnd, hdc)
+                rgb_int = None
+        elif cls.config["method"] == 2:
+            try:
+                rgb_int = parse_rbg_tuple2int(pag.pixel(*coordinate))
+            except OSError:
+                rgb_int = None
+            except Exception:
+                raise
 
         return rgb_int
 
@@ -57,19 +65,21 @@ class PixelDetector:
     def tell_pixel_info(cls):
         def pretty_format(it, space):
             return ", ".join((f"{s:>{space}}" for s in it))
+        try:
+            pixel_info = cls.cur_pixel_info.copy()
+            hwnd_client = pixel_info['hwnd_client']
+            coordinate_global_to_use = "coordinate_global_mod" if cls.config["adjust_coordinate"] else "coordinate_global"
+            text_coordinate_global = pretty_format(pixel_info[coordinate_global_to_use], 4)
+            text_coordinate_client = pretty_format(pixel_info["coordinate_client"], 4)
+            text_rgb_tuple = pretty_format(pixel_info["rgb_tuple"] or ('', '', ''), 3)
 
-        pixel_info = cls.cur_pixel_info.copy()
-        hwnd_client = pixel_info['hwnd_client']
-        coordinate_global_to_use = "coordinate_global_mod" if cls.config["adjust_coordinate"] else "coordinate_global"
-        text_coordinate_global = pretty_format(pixel_info[coordinate_global_to_use], 4)
-        text_coordinate_client = pretty_format(pixel_info["coordinate_client"], 4)
-        text_rgb_tuple = pretty_format(pixel_info["rgb_tuple"] or ('', '', ''), 3)
-
-        print("=" * 128)
-        print(f"【{win32gui.GetWindowText(hwnd_client)}】@{hwnd_client} @{pixel_info['monitor']}")
-        print(f"Global: ({text_coordinate_global})  "
-              f"Client: ({text_coordinate_client})  "
-              f"RGB: ({text_rgb_tuple}) {rgb_int if (rgb_int := pixel_info['rgb_int']) is not None else ''}")
+            print("=" * 128)
+            print(f"【{win32gui.GetWindowText(hwnd_client)}】@{hwnd_client} @{pixel_info['monitor']}")
+            print(f"Global: ({text_coordinate_global})  "
+                  f"Client: ({text_coordinate_client})  "
+                  f"RGB: ({text_rgb_tuple}) {rgb_int if (rgb_int := pixel_info['rgb_int']) is not None else ''}")
+        except Exception as e:
+            print(e)
 
     @classmethod
     def copy_pixel_info(cls):
@@ -82,6 +92,7 @@ class PausableThread(Thread):
         super().__init__(*args, **kwargs)
         self.status_can_run = Event()
         self.interval = 1
+        self.pixel_detector = PixelDetector
 
     def start(self) -> None:
         super().start()
@@ -91,8 +102,8 @@ class PausableThread(Thread):
         while True:
             self.status_can_run.wait()
             try:
-                PixelDetector.update_pixel_info()
-                PixelDetector.tell_pixel_info()
+                self.pixel_detector.update_pixel_info()
+                self.pixel_detector.tell_pixel_info()
                 time.sleep(self.interval)
             finally:
                 pass
@@ -113,6 +124,11 @@ class PausableThread(Thread):
         self.interval = float(input("set a new interval: "))
         self.status_can_run.set()
 
+    def set_method(self):
+        self.status_can_run.clear()
+        self.pixel_detector.config["method"] = int(input("set a new method(1-all screen; 2-current screen): "))
+        self.status_can_run.set()
+
 
 class Manager(Thread):
     def __init__(self):
@@ -129,6 +145,7 @@ class Manager(Thread):
     def run(self) -> None:
         binding_ops = {
             "f4": PixelDetector.copy_pixel_info,
+            "f7": self._job_thread.set_method,
             "f8": self._job_thread.set_interval,
             "f12": lambda: self._job_thread.resume() if self._job_thread.is_paused() else self._job_thread.pause(),
         }
