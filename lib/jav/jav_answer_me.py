38e77ea9297ecd338358dc131f16c89470741781
ShaoyenYu <shaoyenyu@gmail.com>
2022-08-08 02:27:25 +0800

    [ENH,REF] refactor `jav_answer_me` module --- + implement `JavLibrary` low level and combined api in class `JavLibraryApi` + now parse video information base on page table rows instead of page title + support fetching and previewing video thumbnail

--- lib/jav/jav_answer_me.py
+++ lib/jav/jav_answer_me.py
@@ -1,83 +1,140 @@
 import re
+import time
+from io import BytesIO
+from typing import Dict, Optional
 
 import requests
-
-from util.win32.common import set_text
-
-session = requests.session()
-session.headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36",
-    "Referer": "http://www.h28o.com/cn/",
-    "Proxy-Connection": "keep-alive",
-}
-session.proxies = {
-    "https": "socks5://127.0.0.1:10808",
-    "http": "socks5://127.0.0.1:10808"
-}
+from PIL import Image
+from lxml import etree
+from matplotlib import pyplot as plt
 
 
-def search(serie_no=None):
-    serie_no = re.sub("-| |\.", "", serie_no)
-    serie_no = "-".join((
-        "".join([x for x in serie_no if x.isalpha()]),
-        "".join([x for x in serie_no if x.isdigit()]),
-    )).upper()
+def parse_serial_no(serial_no) -> Optional[str]:
+    patt = "(?<![a-zA-Z])([a-zA-Z]{3,6})-?(\d{3,10})"
+    if (matched := re.search(patt, serial_no)) is not None:
+        serial_no_alpha, serial_no_num = matched.groups()
+        return "-".join((serial_no_alpha, serial_no_num)).upper()
+    return None
 
-    print("parsing...")
-    base_url = "http://www.javlibrary.com/cn"
-    api_search, api_detail = "vl_searchbyid.php?keyword=", "?v="
 
-    api = f"{base_url}/{api_search}{serie_no}"
+class JavLibraryApi:
+    base_url = "https://www.javlibrary.com/cn"
 
-    print(api)
-    resp = session.get(api)
+    def __init__(self):
+        self.session = self.prepare_session()
 
-    def check_and_redirect(resp):
-        if not resp.url.startswith(f"{base_url}/{api_detail}"):
-            res_candidates = re.findall("\<a href=\".*?v=(.*?)\" title=(.*?)\"\>\<div class=\"id\"\>(.*?)\<", resp.text)
-            print(res_candidates)
-            res_candidates = {x[2]: x[0] for x in res_candidates}
+    @staticmethod
+    def prepare_session():
+        session = requests.session()
+        session.headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36",
+            "Referer": "https://www.javlibrary.com/cn/",
+            "Proxy-Connection": "keep-alive",
+        }
+        session.proxies = {
+            "https": "socks5://127.0.0.1:10808",
+            "http": "socks5://127.0.0.1:10808"
+        }
+        return session
 
-            return session.get(f"{base_url}/{api_detail}{res_candidates[serie_no]}")
+    def impl_search_by_keyword(self, serial_no):
+        url_video_list = f"{self.base_url}/vl_searchbyid.php?keyword={serial_no}"
+        resp = self.session.get(url_video_list, allow_redirects=False)
         return resp
 
-    def parse_title(resp):
-        whole_title = re.search("<title>(.*?)</title>", resp.text).group(1)
-        whole_title = whole_title.replace(" - JAVLibrary", "")
-        serie_no, content = whole_title.split(" ", maxsplit=1)
+    def impl_get_video_detail(self, video_id):
+        video_detail = f"{self.base_url}/?v={video_id}"
+        print(video_detail)
+        return self.session.get(video_detail)
 
-        context_rev_splitted = content[::-1].split(" ", maxsplit=1)
-        if len(context_rev_splitted) == 2:
-            actress, title = (x[::-1] for x in context_rev_splitted)
+    def search_by_keyword(self, serial_no) -> Optional[Dict[str, str]]:
+        resp = self.impl_search_by_keyword(serial_no)
+
+        patt_video_id = re.compile(r"\.\/\?v=(.*)")
+        res = {}
+        if resp.status_code == 200:
+            video_urls = etree.HTML(resp.text).xpath(".//div[@class='videos']/div[@class='video']/a")
+            res = {
+                parse_serial_no(video_url.get("title")): patt_video_id.match(video_url.get("href")).group(1)
+                for video_url in video_urls
+            }
+        elif resp.status_code == 302:
+            video_id = patt_video_id.match(resp.headers["location"]).group(1)
+            res[serial_no] = video_id
         else:
-            title, = (x[::-1] for x in context_rev_splitted)
-            actress = None
+            raise
+        return res
 
-        return serie_no, actress, title
+    def get_video_detail(self, serial_no_reg, with_thumbnail=False):
+        video_id = self.search_by_keyword(serial_no_reg)[serial_no_reg]
+        resp_video_detail = self.impl_get_video_detail(video_id)
 
-    def parse_date(resp):
-        return re.search("发行日期.*?(\d{4}-\d{2}-\d{2})", re.sub("\s", "", resp.text)).group(1)
+        root = etree.HTML(resp_video_detail.text)
+        title = root.xpath(".//div[@id='video_title']//a[@rel='bookmark']/text()")[0]
 
-    resp = check_and_redirect(resp)
-    serie_no, actress, title = parse_title(resp)
-    release_date = parse_date(resp).replace("-", ".")
+        video_infos = dict(zip(
+            ("serial_no", "publish_date", "length", "director", "maker", "publisher", "review", "category", "cast"),
+            root.xpath(".//div[@id='video_info']/div")
+        ))
+        serial_no = video_infos["serial_no"].xpath(".//td[@class='text']/text()")[0]
+        issue_date = video_infos["publish_date"].xpath(".//td[@class='text']/text()")[0]
+        length = video_infos["length"].xpath(".//span[@class='text']/text()")[0]
+        maker = video_infos["maker"].xpath(".//td[@class='text']/span/a/text()")[0].strip()
+        publisher = video_infos["publisher"].xpath(".//td[@class='text']/span/a/text()")[0]
+        casts = video_infos["cast"].xpath(".//td[@class='text']//span[@class='star']/a[@rel='tag']/text()")
 
-    return serie_no.upper(), actress, title, release_date
+        url_thumbnail = root.xpath(".//img[@id='video_jacket_img']/@src")[0]
+        if not url_thumbnail.startswith("https:"):
+            url_thumbnail = f"https:{url_thumbnail}"
+        thumbnail = Image.open(BytesIO(requests.get(url_thumbnail).content)) if with_thumbnail else None
+
+        res = {
+            "title": title, "serial_no": serial_no, "publish_date": issue_date, "length": length,
+            "maker": maker, "publisher": publisher, "cast": sorted(casts),
+            "thumbnail": thumbnail
+
+        }
+
+        return res
 
 
-def main():
-    import time
+class JavRecognizer:
+    def __init__(self):
+        self.jav_api = JavLibraryApi()
+
+    def recognize(self, serial_no, with_thumbnail=True):
+        serial_no_reg = parse_serial_no(serial_no)
+        video_info = self.jav_api.get_video_detail(serial_no_reg, with_thumbnail)
+        return video_info
+
+
+def run():
+    plt.rcParams["font.sans-serif"] = ["SimHei"]
+    plt.rcParams["axes.unicode_minus"] = False
+    with_thumbnail = True if input("Download thumbnail? y(yes), n(no)") == "y" else False
+
+    recognizer = JavRecognizer()
     while True:
         try:
-            no = str(input("#" * 64 + "\n"))
-            if no == "?":
+            serial_no = str(input("#" * 64 + "\n"))
+            if serial_no == "?":
                 break
 
-            serie_no, actress, title, release_date = search(no)
+            video_info = recognizer.recognize(serial_no, with_thumbnail=with_thumbnail)
+            serial_no, casts, title, release_date, thumbnail = (
+                video_info[k] for k in ("serial_no", "cast", "title", "publish_date", "thumbnail")
+            )
+            title = title.replace(serial_no, "").strip()
+            casts = "&".join(casts)
 
-            text = f"{release_date} {serie_no} {title}"
-            set_text(text)
-            print(actress, text)
+            text = f"{casts}_{release_date}_{serial_no}_{title}"
+
+            if with_thumbnail:
+                plt.imshow(thumbnail)
+                plt.title(text)
+                plt.show()
+
+            print(text)
             print("#" * 64, "\n")
             time.sleep(1)
         except Exception as e:
@@ -86,4 +143,4 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    run()
